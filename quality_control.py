b418c96b81ba771f8c64ac01bd1c8dbf5a11fd2c
yesse monkou <monkou.yesse@gmail.com>
2024-12-05 15:25:08 +0100

    Documentatie quality_control.py

--- quality_control.py
+++ quality_control.py
@@ -3,7 +3,7 @@ import math
 import json
 
 
-def top_five(seq, nor, avg_pps, header, subject, marker):
+def top_five(seq: str, nor: int, avg_pps: float, header: str, subject: list[tuple[int, int, dict]], marker: int) -> list[tuple[int, int, dict]]:
     if len(subject) < 5:
         read_data = (marker, nor, {"read_length": len(seq),
                                          "phred": avg_pps,
@@ -19,7 +19,7 @@ def top_five(seq, nor, avg_pps, header, subject, marker):
     return subject
 
 
-def distrubutions(marker, subject):
+def distrubutions(marker: int, subject: dict) -> dict:
     afronding = str(marker)
     if afronding in subject:
         subject[afronding] += 1
@@ -29,66 +29,71 @@ def distrubutions(marker, subject):
     return subject
 
 
-def read_fastq(fq_file_path, js_file_path):
-    base_counts = {"a":0,"c":0,"t":0,"g":0}
-    number_of_reads = 0
-    total_score = 0
-    read_length_distribution = {}
-    phred_distribution = {}
-    longest_five_ = []
-    best_five_ = []
+def read_fastq(fq_file_path: str, js_file_path: str) -> None:
+    """
+    Fungeert als de main voor de quality control
+    """
+    
+    base_counts: dict = {"a":0,"c":0,"t":0,"g":0}
+    number_of_reads: int = 0
+    total_score: int = 0
+    read_length_distribution: dict = {}
+    phred_distribution: dict = {}
+    longest_five_: list = []  
+    best_five_: list = [] 
 
     with open(fq_file_path, 'r') as file:
         while True:
-            header = file.readline().strip()
+            header: str = file.readline().strip()
             if not header:
                 break
-            sequence = file.readline().strip()
+            sequence: str= file.readline().strip()
             file.readline()
-            quality = file.readline().strip()
+            quality:str = file.readline().strip()
 
             for basen in sequence:
                 base_counts[basen.lower()] +=1
 
             number_of_reads +=1
 
-            phred_scores = [ord(char) - 33 for char in quality]
-            p_values = [10 ** (-score / 10) for score in phred_scores]
+            phred_scores: list[int] = [ord(char) - 33 for char in quality]
+            p_values: list[float] = [10 ** (-score / 10) for score in phred_scores]
             total_score += sum(p_values)
-            avg_phred_per_sequence = -10 * math.log10(sum(p_values) / len(
+            avg_phred_per_sequence: float = -10 * math.log10(sum(p_values) / len(
                 sequence))
 
-            longest_five_ = top_five(sequence, number_of_reads,
+            longest_five_: list[tuple[int, int, dict]] = top_five(sequence, number_of_reads,
                                     avg_phred_per_sequence, header,
                                     longest_five_, len(sequence))
 
-            best_five_ = top_five(sequence, number_of_reads,
+            best_five_: list[tuple[int, int, dict]] = top_five(sequence, number_of_reads,
                                     avg_phred_per_sequence, header,
                                     best_five_, avg_phred_per_sequence)
 
-            read_length_distribution = distrubutions(math.ceil((len(
+            read_length_distribution: dict = distrubutions(math.ceil((len(
                 sequence)+1)/1000), read_length_distribution)
 
-            phred_distribution = distrubutions(int(round(
+            phred_distribution: dict = distrubutions(int(round(
                 avg_phred_per_sequence)), phred_distribution)
 
 
-    total_bases = sum(base_counts.values())
-    gc_ratio = (base_counts["g"]+base_counts["c"])/total_bases
-    avg_phred = -10 * math.log10(total_score / total_bases)
-    avg_length = total_bases / number_of_reads
-    longest_five_ = sorted(longest_five_, key=lambda x: x[2]["read_length"],
+    total_bases: int = sum(base_counts.values())
+    gc_ratio: float = (base_counts["g"]+base_counts["c"])/total_bases
+    avg_phred: float = -10 * math.log10(total_score / total_bases)
+    avg_length: float = total_bases / number_of_reads
+    longest_five_: list[tuple[int, int, dict]] = sorted(longest_five_, key=lambda x: x[2]["read_length"],
                            reverse=True)
-    best_five_ = sorted(best_five_, key=lambda x: x[2]["phred"], reverse=True)
+    best_five_: list[tuple[int, int, dict]] = sorted(best_five_, key=lambda x: x[2]["phred"], reverse=True)
 
-    longest_five = []
-    best_five = []
+    longest_five: list = []
+    best_five: list = []
+
     for reads in longest_five_:
         longest_five.append(reads[2])
     for reads in best_five_:
         best_five.append(reads[2])
 
-    data = {"base_counts": base_counts,
+    data: dict = {"base_counts": base_counts,
             "gc_ratio": gc_ratio,
             "number_of_reads": number_of_reads,
             "total_bases": total_bases,
@@ -102,7 +107,9 @@ def read_fastq(fq_file_path, js_file_path):
     with open(js_file_path, 'w') as file:
         json.dump(data, file, indent=4)
 
-fastq_file_path = snakemake.input.fastq
-json_file_path = snakemake.output.json
-read_fastq(fastq_file_path, json_file_path)
 
+if __name__ == "__main__":
+    fastq_file_path: str = snakemake.input.fastq
+    json_file_path: str = snakemake.output.json
+
+    read_fastq(fastq_file_path, json_file_path)
